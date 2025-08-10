063e3f68a25fccd14611c443e0ed95cf28bc0382
guyt6517 <guyt6516@gmail.com>
2025-08-09 22:00:13 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 import os
 from flask import Flask, jsonify, request, abort, render_template_string, session
 import random
+from flask_cors import CORS
 
 app = Flask(__name__)
+
+CORS(app)
 
 DB_FILE = "db.txt"
 
